86dca501d763556f6620574b875e17c1386e5c49
Bora Alp Arat <bora@alparat.com>
2014-12-24 20:26:23 +0200

    Update css.php

--- css.php
+++ css.php
@@ -8,14 +8,18 @@ $files= array(
 );
 $output='';
 foreach($files as $file){
-  $output.=file_get_contents($file);
+        $output.=file_get_contents($file);
 }
-$output=preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $output);
+$output=preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!','',$output);
 $output=str_replace(': ', ':', $output);
-$output=str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $output);
+$output=str_replace(' :', ':', $output);
+$output=str_replace(' {', '{', $output);
+$output=str_replace('{ ', '{', $output);
+$output=trim($output);
+$output=str_replace(array('\r\n','\r','\n','\t','  ','    ','    ','      ','        '), '', $output);
 ob_start('ob_gzhandler');
 header('Cache-Control: public');
-header('Expires: '. gmdate('D, d M Y H:i:s', time() + 86400) . ' GMT');
+header('Expires: '.gmdate('D, d M Y H:i:s', time() + 2592000) . ' GMT');
 header('Content-type: text/css');
 echo($output);
 ?>
